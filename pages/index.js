4f124fe6ad9c032e0737db367abdbd9c523a7cb5
Tristan Hanna <tristan.hanna@colorado.edu>
2020-04-29 22:48:40 -0500

    favicon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="container">
       <Head>
         <title>Tristan Hanna</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <style>
                     @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap');
